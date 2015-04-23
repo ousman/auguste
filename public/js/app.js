00a7a6b2780c347d9df56e38a9d393582a771c2d
Tidio <tidiany.pro@gmail.com>
2015-04-23 20:38:03 +0200

    the end

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('#series').on('change', function () {
+        $('#tags').prop('selectedIndex',0);
         var filter = '.'+$('#series').find(":selected").text().toLowerCase();
         $('#series').attr('data-filter', filter);
         $('#series').removeClass('active');
@@ -8,6 +9,7 @@ $(document).ready(function () {
     });
     
         $('#tags').on('change', function () {
+        $('#series').prop('selectedIndex',0);
         var filter = '.'+$('#tags').find(":selected").text().toLowerCase();
         $('#tags').attr('data-filter', filter);
         $('#tags').removeClass('active');
