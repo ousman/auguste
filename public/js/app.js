09b20ffe4b7d46e9c0ecbbec7fe8345919cd3043
Tidio <tidiany.pro@gmail.com>
2015-06-27 19:37:21 +0200

    password

--- public/js/app.js
+++ public/js/app.js
@@ -18,30 +18,31 @@ $(document).ready(function () {
         console.log(filter);
     });
 
-    $('.filter').click(function () {
-        var webroot = $('#webroot').val();
-        console.log($(this).text());
-        $.ajax({
-                url: webroot + 'manage/getTagsByName/',
-                type: 'POST',
-                data: {serie: $(this).text()},
-                dataType: 'JSON',
-                success: function (data) {
-                    console.log('reussi');
-                    console.log(data);
-                    $('#menu-filter').detach();
-                    $('.filters').append('<ul class="w-list-unstyled filter-list" id="menu-filter"><li class="filter-iterm"><a class="filter" href="#" data-filter="all">All</a></li>');
-                    var d;
-                    for (d in data) {
-                        console.log(d);
-                        $('#menu-filter').append('<li class="filter-iterm"><a class="filter" href="#" data-filter=".' + data[d]['Label'] + '">' + data[d]['Label'] + '</a></li>');
-                    }
-                    $('#filters').append('</ul>')
-                },
-                error: function (e) {
-                    console.log(e);
-                    console.log('erreur');
-                }});
-    })
+
+//    $('.filter').click(function () {
+//        var webroot = $('#webroot').val();
+//        console.log($(this).text());
+//        $.ajax({
+//                url: webroot + 'manage/getTagsByName/',
+//                type: 'POST',
+//                data: {serie: $(this).text()},
+//                dataType: 'JSON',
+//                success: function (data) {
+//                    console.log('reussi');
+//                    console.log(data);
+//                    $('#menu-filter').detach();
+//                    $('.filters').append('<ul class="w-list-unstyled filter-list" id="menu-filter"><li class="filter-iterm"><a class="filter" href="#" data-filter="all">All</a></li>');
+//                    var d;
+//                    for (d in data) {
+//                        console.log(d);
+//                        $('#menu-filter').append('<li class="filter-iterm"><a class="filter" href="#" data-filter=".' + data[d]['Label'] + '">' + data[d]['Label'] + '</a></li>');
+//                    }
+//                    $('#filters').append('</ul>')
+//                },
+//                error: function (e) {
+//                    console.log(e);
+//                    console.log('erreur');
+//                }});
+//    })
 
 });
